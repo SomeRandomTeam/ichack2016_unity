56bee2eabd294cac6bf84c6ab688d294cdb36c5c
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-02-20 14:45:24 +0000

    Basic Temp working.

---
+++ Desky/Assets/Stream.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var url = "http://mishrabhinav.com/assets/img/avatar.jpg";
+
+function Start () {
+	// Create a texture in DXT1 format
+	GetComponent.<Renderer>().material.mainTexture = new Texture2D(4, 4, TextureFormat.DXT1, false);
+	while(true) {
+		// Start a download of the given URL
+		var www = new WWW(url);
+
+		// wait until the download is done
+		yield www;
+
+		// assign the downloaded image to the main texture of the object
+		www.LoadImageIntoTexture(GetComponent.<Renderer>().material.mainTexture);
+	}
+}
+
+function Update () {
+
+	url = "http://mishrabhinav.com/assets/img/mymos.jpg";
+}
