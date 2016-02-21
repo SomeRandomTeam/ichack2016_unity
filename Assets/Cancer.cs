f21629fd5cfac34ad92660238c37e566dd1c2789
Szilveszter Székely <szekelyszilv@gmail.com>
2016-02-21 04:54:47 +0000

    Decancer cancer a bit

--- Assets/Cancer.cs
+++ Assets/Cancer.cs
@@ -3,57 +3,75 @@ using System.Collections;
 
 public class Cancer : MonoBehaviour {
 
+	private const float updateFrequency = 1.0f;
+
 	public bool mainScreen = false;
 
-	public string url = "http://192.168.12.1:9999/";
-	public string newRequest = "http://192.168.12.1:9999/switch/0";
+	public string url;
+	public string switchRequestUrl;
 
-	private bool firstLoad = true;
-	private double delay = 0.0d;
+	private double nextGrabIn = 0.0f;
 
 	private CardboardHead head;
 
 	// Use this for initialization
 	IEnumerator Start () {
 		head = Camera.main.GetComponent<StereoController> ().Head;
-
+		yield return StartCoroutine (GrabScreen ());
 		yield break;
-
 	}
 
 
 	// Update is called once per frame
 	void Update () {
-		RaycastHit hit;
-		bool isLookedAt = GetComponent<Collider> ().Raycast (head.Gaze, out hit, Mathf.Infinity);
+		bool isLookedAt = IsLookedAt ();
 		//Debug.Log (isLookedAt);
-		if (firstLoad) {
-			firstLoad = false;
-			StartCoroutine (display ());
-		} else if (Time.time > delay && isLookedAt) {
-			  delay = Time.time + 2.0d;
-			  StartCoroutine (display ());
+		if (isLookedAt) {
+			StartCoroutine (SwitchTo ());
+			// First detect trigger then magnify
+			if (Cardboard.SDK.Triggered) {
+				Magnify ();
+			}
+
+			//Then look for time passed and refresh
+			nextGrabIn -= Time.deltaTime;
+			if (nextGrabIn <= 0.0f) {
+				StartCoroutine (GrabScreen ());
+				nextGrabIn = updateFrequency;
+			}
 		}
 	}
 
-	IEnumerator display() {
-		WWW nwww = new WWW (newRequest);
+	bool IsLookedAt() {
+		RaycastHit hit;
+		return GetComponent<Collider> ().Raycast (head.Gaze, out hit, Mathf.Infinity);
+	}
+
+	void Magnify() {
+		Camera.main.transform.position = new Vector3 (-110, 0, 0);
+	}
+
+	IEnumerator SwitchTo() {
+		WWW nwww = new WWW (switchRequestUrl);
 		yield return nwww;
+		yield break;
+	}
+
+	IEnumerator GrabScreen() {
 		WWW www = new WWW (url);
 		yield return www;
-		Renderer renderer = GetComponent<Renderer> ();
-		Texture2D tex = null;
+
 		try {
-			tex = www.texture;
+			Texture2D tex = www.texture;
+			Renderer renderer = GetComponent<Renderer> ();
+			if (tex != null) {
+				renderer.material.mainTexture = tex;
+			}
 		} catch {
 			Debug.Log ("Failed to load image");
 		}
-		if (tex != null) {
-			renderer.material.mainTexture = tex;
-		}
-		delay = Time.time + 1.0d;
 
+		nextGrabIn = Time.time + 1.0d;
+		yield break;
 	}
-
-
 }
